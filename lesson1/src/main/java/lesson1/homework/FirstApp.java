e401f374ffd4f53b806e456a14e7aaedb5685dc8
veronikaminchjonak <veronichka_@list.ru>
2022-06-08 20:50:20 +0200

    First Commit

---
+++ lesson1/src/main/java/lesson1/homework/FirstApp.java
@@ -0,0 +1,10 @@
+package lesson1.homework;
+
+/**
+ * First JAVA program.
+ */
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
